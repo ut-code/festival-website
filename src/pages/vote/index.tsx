e3a5af445915ec0d8f4d610ce4b6843df50ebec2
ski.com <100414605+ski-dot-com@users.noreply.github.com>
2025-11-21 03:11:02 +0900

    [fix] vote関連のことを軒並み修正
    後はHacker-shooterの対応だけ

--- src/pages/vote/index.tsx
+++ src/pages/vote/index.tsx
@@ -20,13 +20,13 @@ export default function Home(): JSX.Element {
       <main className="container padding-vert--lg">
         <p>
           本日は企画にお越しいただきありがとうございます。次のQRコードまたは
-          <a href="https://gogatsusai.jp/98/visitor/project/261">
+          <a href="https://www.komabasai.net/76/visitor/project/312">
             こちらのリンク
           </a>
           から五月祭グランプリへの投票をお願いしております。1分ほどで完了しますので、次の動画を参考に投票をお願いします！
         </p>
         <div className={styles.container}>
-          <img src="/img/mf98-vote.png" width={"500px"} height={"500px"}></img>
+          <img src="/img/kf76-vote.png" width={"500px"} height={"500px"}></img>
           <video
             src={howToVoteVideo}
             controls
