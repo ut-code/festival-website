db90c77ef1354161397d1a6cb412fbe65afb54e5
ski.com <100414605+ski-dot-com@users.noreply.github.com>
2025-11-21 03:29:23 +0900

    [feat] QRコードを飾り付ける
    TODO: 動画の差し替え

--- src/pages/vote/index.tsx
+++ src/pages/vote/index.tsx
@@ -26,7 +26,16 @@ export default function Home(): JSX.Element {
           から五月祭グランプリへの投票をお願いしております。1分ほどで完了しますので、次の動画を参考に投票をお願いします！
         </p>
         <div className={styles.container}>
-          <img src="/img/kf76-vote.png" width={"500px"} height={"500px"}></img>
+          <div className={styles.QRCodeContainer}>
+            <strong>↓↓↓↓↓↓↓↓↓↓</strong>
+            <img
+              src="/img/kf76-vote.png"
+              width={"500px"}
+              height={"500px"}
+              className={styles.QRCodeImage}
+            ></img>
+            <strong>↑↑↑↑↑↑↑↑↑↑</strong>
+          </div>
           <video
             src={howToVoteVideo}
             controls
