fcc28a858dc03c4ab51fdb83684aa096ff4360ea
ski.com <100414605+ski-dot-com@users.noreply.github.com>
2025-11-22 09:15:10 +0900

    [fix] <BrowserOnly>を導入してエラーを回避
    こいつサーバーサイドでレンダリングしてたのかよ…

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,10 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import Heading from "@theme/Heading";
 import { exhibitions } from "../exhibitions.ts";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 
 const trackerURL = "https://" + "tracker.ut-code.workers" + ".dev";
 export default function Home(): JSX.Element {
-  const is_offline = !!localStorage.getItem("is_offline");
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout>
@@ -18,82 +18,93 @@ export default function Home(): JSX.Element {
         </div>
       </header>
       <main className="container padding-vert--lg">
-        <ul
-          className="row row--no-gutters"
-          style={{
-            padding: 0,
-            listStyleType: "none",
+        <BrowserOnly>
+          {() => {
+            const is_offline = !!localStorage.getItem("is_offline");
+            return (
+              <ul
+                className="row row--no-gutters"
+                style={{
+                  padding: 0,
+                  listStyleType: "none",
+                }}
+              >
+                {exhibitions.map((ex) => (
+                  <li key={ex.title} className="col col--6 padding--md">
+                    <div className="card" style={{ height: "100%" }}>
+                      <div
+                        className="card__image"
+                        style={{
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }}
+                      >
+                        <img
+                          style={{
+                            maxWidth: "100%",
+                            maxHeight: "400px",
+                            objectFit: "contain",
+                          }}
+                          src={ex.image.src}
+                          alt={ex.image.alt}
+                        />
+                      </div>
+                      <div className="card__body">
+                        <h3>{ex.title}</h3>
+                        {ex.isNew && (
+                          <h3 className="badge badge--success">NEW</h3>
+                        )}
+                        <h3 className="badge badge--primary">{ex.target}</h3>
+                        <h3 className="badge badge--info">{ex.takes}</h3>
+                        <p>{ex.description}</p>
+                      </div>
+                      <div className="card__footer">
+                        <button
+                          type="button"
+                          disabled={
+                            ex.disabled || (ex.offline_only && !is_offline)
+                          }
+                          onClick={async () => {
+                            console.log(`clicked ${ex.title}`);
+                            const url = ex.url;
+                            if (url) {
+                              window.open(url, "_blank").focus();
+                              // CORS の関係でエラーが出るが、特に問題ないので放置。
+                              try {
+                                await fetch(trackerURL, {
+                                  method: "POST",
+                                  body: JSON.stringify({
+                                    url,
+                                    key: "2e0c7cad39e09314a46f217c6107f96e08bd13984cd4ae4c29d96f5db440dba8",
+                                    kind: "festival",
+                                  }),
+                                });
+                              } catch (err) {}
+                            } else {
+                              const dialog = ex.dialog;
+                              if (dialog) {
+                                alert(dialog);
+                              }
+                            }
+                          }}
+                          rel="noreferrer"
+                          className="button button--primary button--block"
+                        >
+                          {ex.disabled
+                            ? "準備中…"
+                            : ex.offline_only && !is_offline
+                              ? "実地のみ"
+                              : "この企画を体験する"}
+                        </button>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            );
           }}
-        >
-          {exhibitions.map((ex) => (
-            <li key={ex.title} className="col col--6 padding--md">
-              <div className="card" style={{ height: "100%" }}>
-                <div
-                  className="card__image"
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <img
-                    style={{
-                      maxWidth: "100%",
-                      maxHeight: "400px",
-                      objectFit: "contain",
-                    }}
-                    src={ex.image.src}
-                    alt={ex.image.alt}
-                  />
-                </div>
-                <div className="card__body">
-                  <h3>{ex.title}</h3>
-                  {ex.isNew && <h3 className="badge badge--success">NEW</h3>}
-                  <h3 className="badge badge--primary">{ex.target}</h3>
-                  <h3 className="badge badge--info">{ex.takes}</h3>
-                  <p>{ex.description}</p>
-                </div>
-                <div className="card__footer">
-                  <button
-                    type="button"
-                    disabled={ex.disabled || (ex.offline_only && !is_offline)}
-                    onClick={async () => {
-                      console.log(`clicked ${ex.title}`);
-                      const url = ex.url;
-                      if (url) {
-                        window.open(url, "_blank").focus();
-                        // CORS の関係でエラーが出るが、特に問題ないので放置。
-                        try {
-                          await fetch(trackerURL, {
-                            method: "POST",
-                            body: JSON.stringify({
-                              url,
-                              key: "2e0c7cad39e09314a46f217c6107f96e08bd13984cd4ae4c29d96f5db440dba8",
-                              kind: "festival",
-                            }),
-                          });
-                        } catch (err) {}
-                      } else {
-                        const dialog = ex.dialog;
-                        if (dialog) {
-                          alert(dialog);
-                        }
-                      }
-                    }}
-                    rel="noreferrer"
-                    className="button button--primary button--block"
-                  >
-                    {ex.disabled
-                      ? "準備中…"
-                      : ex.offline_only && !is_offline
-                        ? "実地のみ"
-                        : "この企画を体験する"}
-                  </button>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+        </BrowserOnly>
       </main>
     </Layout>
   );
