d43ce132235444555020d95aa32a1244cc8034aa
TKHR-Shiu <math-science1357@ymail.ne.jp>
2026-05-14 18:16:48 +0900

    投票の写真・リンクを変更

--- src/pages/vote/index.tsx
+++ src/pages/vote/index.tsx
@@ -20,13 +20,13 @@ export default function Home(): JSX.Element {
       <main className="container padding-vert--lg">
         <p>
           本日は企画にお越しいただきありがとうございます。次のQRコードまたは
-          <a href="https://gogatsusai.jp/98/visitor/project/261">
+          <a href="https://visitor.gogatsusai.jp/project/501?action=vote&utm_source=qr&utm_medium=project-flyer&utm_campaign=project-flyer-evoke">
             こちらのリンク
           </a>
           から五月祭グランプリへの投票をお願いしております。1分ほどで完了しますので、次の動画を参考に投票をお願いします！
         </p>
         <div className={styles.container}>
-          <img src="/img/mf98-vote.png" width={"500px"} height={"500px"}></img>
+          <img src="/img/mf99-vote.png" width={"500px"} height={"500px"}></img>
           <video
             src={howToVoteVideo}
             controls
